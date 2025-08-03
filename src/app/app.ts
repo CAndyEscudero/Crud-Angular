3c3bbafb01c0da00f40202c92007f78b50c21869
CAndyEscudero <emula2k@gmail.com>
2025-08-03 18:43:55 -0300

    agregando barra navegacion

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,18 +1,32 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSlideToggleModule, MatSlideToggleChange } from '@angular/material/slide-toggle';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { MatTableModule } from '@angular/material/table'
+import { BarraNavegacionComponent } from './barra-navegacion/barra-navegacion.component';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [
+    RouterOutlet,
+    MatSlideToggleModule,
+    MatTableModule,
+    BarraNavegacionComponent
+  ],
+  providers: [provideAnimations()],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrls: ['./app.css'],
 })
 export class App {
   protected title = 'angular-crud';
   nombre = 'Andres';
   city = 'Argentina';
-}
 
-export class Users {
-  username = 'Andres';
+  toggleChanged(event: MatSlideToggleChange) {
+    console.log('Toggle está', event.checked ? 'activado' : 'desactivado');
+
+  }
 }
---
+++ src/app/barra-navegacion/barra-navegacion.component.spec.ts
@@ -0,0 +1,31 @@
+import { waitForAsync, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { BarraNavegacionComponent } from './barra-navegacion.component';
+
+describe('BarraNavegacionComponent', () => {
+  let component: BarraNavegacionComponent;
+  let fixture: ComponentFixture<BarraNavegacionComponent>;
+
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+      declarations: [BarraNavegacionComponent],
+      imports: [
+        MatButtonModule,
+        MatIconModule,
+        MatListModule,
+        MatSidenavModule,
+        MatToolbarModule,
+      ]
+    });
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(BarraNavegacionComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should compile', () => {
+    expect(component).toBeTruthy();
+  });
+});
